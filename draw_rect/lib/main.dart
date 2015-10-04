63171c04d2457c16a12a62093c3075056c06618c
kyorohiro <kyorohiro@gmail.com>
2015-10-05 01:32:13 +0900

    add draw rect sample

---
+++ draw_rect/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:sky/widgets.dart';
+import 'package:sky/painting.dart';
+import 'package:sky/rendering.dart';
+
+void main() {
+  runApp(new DrawRectWidget());//new GameTest());
+}
+
+class DrawRectWidget extends OneChildRenderObjectWidget {
+    RenderObject createRenderObject() {
+      return new DrawRectObject();
+    }
+}
+
+class DrawRectObject extends RenderBox {
+  void paint(PaintingContext context, Offset offset) {
+    Paint p = new Paint();
+    p.color = new Color.fromARGB(0xff, 0xff, 0xff, 0xff);
+    Rect r = new Rect.fromLTWH(50.0, 100.0, 150.0, 25.0);
+    context.canvas.drawRect(r, p);
+  }
+}
