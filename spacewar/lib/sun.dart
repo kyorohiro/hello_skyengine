95783fc67b2e7559a94becb770313eba22fb6d9a
kyorohiro <kyorohiro@gmail.com>
2015-10-07 23:12:55 +0900

    mod sun interface

--- spacewar/lib/sun.dart
+++ spacewar/lib/sun.dart
@@ -4,17 +4,23 @@ class Sun extends DisplayObject {
   @override
   String objectName = "sun";
 
+  Paint p = new Paint();
+  Color c = new Color.fromARGB(0xaa, 0xff, 0xaa, 0xaa);
+  Rect r = new Rect();
+
+  Sun() {
+    p.color = c;
+  }
+
   @override
   void onInit(Stage stage) {
     this.x = stage.w / 2 + stage.x;
     this.y = stage.h / 2 + stage.y;
+    this.r = new Rect.fromLTWH(x - 5.0, y - 5.0, 10.0, 10.0);
   }
 
   @override
   void onPaint(Stage stage, PaintingCanvas canvas) {
-    Paint paint = new Paint();
-    paint.color = const Color.fromARGB(0xaa, 0xff, 0xaa, 0xaa);
-    Rect r = new Rect.fromLTWH(x - 5.0, y - 5.0, 10.0, 10.0);
-    canvas.drawOval(r, paint);
+    canvas.drawOval(r, p);
   }
 }
