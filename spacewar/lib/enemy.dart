aacd557e1848c685a12d1e5a5d92b2ff1d49e365
kyorohiro <kyorohiro@gmail.com>
2015-10-07 20:54:18 +0900

    ..

--- spacewar/lib/enemy.dart
+++ spacewar/lib/enemy.dart
@@ -8,37 +8,60 @@ class Enemy extends DisplayObject {
   double life = 200.0;
   double size = 50.0;
 
-
   @override
   void onInit(Stage stage) {
-  //  if(this.life > 0) {
-  //    this.x = stage.w * 2/ 3 + stage.x;
-  //    this.y = stage.h * 1/ 3 + stage.y;
-  //  } else {
-      this.x = stage.w * (rand.nextInt(5)+1)/6.0 + stage.x;
-      this.y = stage.h * (rand.nextInt(5)+1)/6.0 + stage.y;
-  //  }
+    //  if(this.life > 0) {
+    //    this.x = stage.w * 2/ 3 + stage.x;
+    //    this.y = stage.h * 1/ 3 + stage.y;
+    //  } else {
+    this.x = stage.w * (rand.nextInt(5) + 1) / 6.0 + stage.x;
+    this.y = stage.h * (rand.nextInt(5) + 1) / 6.0 + stage.y;
+    //  }
     this.life = this.maxlife;
   }
 
   @override
   void onPaint(Stage stage, PaintingCanvas canvas) {
     Paint paint = new Paint();
-    double v = (255.0*life/maxlife);
-    v = (v>0?v:0);
+    double v = (255.0 * life / maxlife);
+    v = (v > 0 ? v : 0);
     paint.color = new Color.fromARGB(0xaa, 0x00, 0x00, v.floor());
-    Rect r = new Rect.fromLTWH(x - size/2, y - size/2, size, size);
+    Rect r = new Rect.fromLTWH(x - size / 2, y - size / 2, size, size);
     canvas.drawOval(r, paint);
   }
 
   int prevTime = 0;
   int numOfBullet = 0;
   void onTick(Stage stage, int timeStamp) {
-    if(timeStamp -prevTime > 500) {
+    if (timeStamp - prevTime > 500) {
       EnemyBullet bullet = new EnemyBullet();
       bullet.x = this.x;
       bullet.y = this.y;
-      bullet.dy = 0.6;
+      switch (numOfBullet % 3) {
+        case 0:
+        SpaceShip spaceship =
+            stage.root.fincObjectFromObjectName("spaceship");
+        if (spaceship != null) {
+          bullet.dx = -1*(spaceship.x - this.x) / 800.0;
+          bullet.dy = (spaceship.y - this.y) / 800.0;
+        }
+        break;
+        case 1:
+          SpaceShip spaceship =
+              stage.root.fincObjectFromObjectName("spaceship");
+          if (spaceship != null) {
+            bullet.dx = (spaceship.x - this.x) / 800.0;
+            bullet.dy = (spaceship.y - this.y) / 800.0;
+          }
+          break;
+        case 2:
+          Sun sun = stage.root.fincObjectFromObjectName("sun");
+          if (sun != null) {
+            bullet.dy = (sun.x - this.x) / 800.0;
+            bullet.dx = -1*(sun.y - this.y) / 800.0;
+          }
+          break;
+      }
       stage.root.addChild(bullet);
       numOfBullet++;
       prevTime = timeStamp;
@@ -47,8 +70,7 @@ class Enemy extends DisplayObject {
 }
 
 class EnemyBullet extends GravityDisplayObject {
-
-  int liveTime = 60*1000;
+  int liveTime = 60 * 1000;
   int birthTime = 0;
 
   EnemyBullet() {
@@ -63,19 +85,18 @@ class EnemyBullet extends GravityDisplayObject {
     canvas.drawOval(r, paint);
 
     SpaceShip spaceship = stage.root.fincObjectFromObjectName("spaceship");
-    if(spaceship != null) {
+    if (spaceship != null) {
       double tx = spaceship.x - this.x;
       double ty = spaceship.y - this.y;
       double distance = math.sqrt(math.pow(tx, 2) + math.pow(ty, 2));
-      if(distance < spaceship.size) {
+      if (distance < spaceship.size) {
         spaceship.life -= 1;
         stage.root.rmChild(this);
       }
     }
     int time = new DateTime.now().millisecondsSinceEpoch;
-    if(time-birthTime > liveTime) {
+    if (time - birthTime > liveTime) {
       stage.root.rmChild(this);
     }
   }
-
 }
