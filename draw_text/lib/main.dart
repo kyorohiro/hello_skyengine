96f9e615eceabcc958325a277d5a61c48bb098a7
kyorohiro <kyorohiro@gmail.com>
2015-10-10 20:16:21 +0900

    mod draw text

--- draw_text/lib/main.dart
+++ draw_text/lib/main.dart
@@ -1,6 +1,31 @@
-import 'package:sky/widgets.dart';
-import 'package:sky/painting.dart';
-import 'package:sky/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:ui' as sky;
 
 void main() {
+  runApp(new DrawTextWidget());
+}
+
+class DrawTextWidget extends OneChildRenderObjectWidget {
+  RenderObject createRenderObject() {
+    return new DrawTextObject();
+  }
+}
+
+class DrawTextObject extends RenderBox {
+  void paint(PaintingContext context, Offset offset) {
+    Color textColor = const Color.fromARGB(0xaa, 0xff, 0, 0);
+    PlainTextSpan textSpan = new PlainTextSpan("Hello Text!! こんにちは!!");
+    TextStyle textStyle = new TextStyle(fontSize: 50.0, color: textColor);
+    StyledTextSpan testStyledSpan = new StyledTextSpan(textStyle, [textSpan]);
+    TextPainter textPainter = new TextPainter(testStyledSpan);
+
+    textPainter.maxWidth = 200; //constraints.maxWidth;
+    textPainter.minWidth = 200; //constraints.minWidth;
+    textPainter.minHeight = constraints.minHeight;
+    textPainter.maxHeight = constraints.maxHeight;
+    textPainter.layout();
+    textPainter.paint(context.canvas, new sky.Offset(100, 100));
+  }
 }
