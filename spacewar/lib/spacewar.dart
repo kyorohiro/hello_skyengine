59242f036a4e62407b5feb9b88c8c86020091bf2
kyorohiro <kyorohiro@gmail.com>
2015-10-10 20:11:17 +0900

    ..

--- spacewar/lib/spacewar.dart
+++ spacewar/lib/spacewar.dart
@@ -1,10 +1,10 @@
 library spacewar;
 
-import 'package:sky/widgets.dart';
-import 'package:sky/rendering.dart';
-import 'package:sky/animation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/animation.dart';
 import 'dart:math' as math;
-import 'dart:sky' as sky;
+import 'dart:ui' as sky;
 import 'dart:async';
 import 'package:vector_math/vector_math_64.dart';
 
