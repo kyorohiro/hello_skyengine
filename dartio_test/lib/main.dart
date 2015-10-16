46c347848bf0fc9b4830246d79730b5058c40c79
kyorohiro <kyorohiro@gmail.com>
2015-10-17 07:25:22 +0900

    dart:io http get post test

---
+++ dartio_test/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/widgets.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+
+main() async {
+  Text t = new Text("${await getTest('http://example.com')}");
+  Center c = new Center(child: t);
+  runApp(c);
+}
+
+Future<String> getTest(String uri) async {
+  HttpClient client = new HttpClient();
+  HttpClientRequest request = await client.getUrl(Uri.parse(uri));
+  HttpClientResponse response = await request.close();
+  StringBuffer builder = new StringBuffer();
+  await for (String a in await response.transform(UTF8.decoder)) {
+    builder.write(a);
+  }
+  return builder.toString();
+}
+
+Future<String> postTest(String uri, String message) async {
+  HttpClient client = new HttpClient();
+  HttpClientRequest request = await client.postUrl(Uri.parse(uri));
+  request.write(message);
+  HttpClientResponse response = await request.close();
+  StringBuffer builder = new StringBuffer();
+  await for (String a in await response.transform(UTF8.decoder)) {
+    builder.write(a);
+  }
+  return builder.toString();
+}
