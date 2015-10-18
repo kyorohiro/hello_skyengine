d55b5fea43174790dd77e23a4f68ef8622eb7bed
kyorohiro <kyorohiro@gmail.com>
2015-10-19 07:33:20 +0900

    widget row

---
+++ widget_row/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+import 'dart:ui' as sky;
+import 'dart:math' as math;
+import 'dart:async';
+
+main() async {
+  double width = 100.0;
+  double height = 100.0;
+  Container c = new Container(
+      child: new Text("Container"),
+      decoration: new BoxDecoration(
+          backgroundColor: new Color.fromARGB(0xaa, 0xff, 0xaa, 0xaa)),
+      width: width,
+      height: height);
+  Text t = new Text("Text");
+  NetworkImage i = new NetworkImage(src: "./icon.jpeg");
+
+  FlexJustifyContent justifyContent = FlexJustifyContent.spaceBetween;
+  FlexAlignItems alignItems = FlexAlignItems.center;
+  TextBaseline textBaseline = TextBaseline.ideographic;
+  runApp(new Row([c, t, i],
+      justifyContent: justifyContent,
+      alignItems: alignItems,
+      textBaseline: textBaseline));
+}
