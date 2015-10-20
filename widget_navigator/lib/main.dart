0fedac868e2894868cd700afd08c21a34d4ae3fd
kyorohiro <kyorohiro@gmail.com>
2015-10-20 09:31:07 +0900

    widget navigator test

---
+++ widget_navigator/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  String title = "TITLE";
+  ThemeData theme = new ThemeData.light();
+  Map<String, RouteBuilder> routes = {
+    "/": (RouteArguments args) => new MyStatlessComponentRoot(args),
+    "/text": (RouteArguments args) => new MyStatlessComponentText(args),
+    "/image": (RouteArguments args) => new MyStatlessComponentImage(args)
+  };
+  MaterialApp app = new MaterialApp(title: title, theme: theme, routes: routes);
+  runApp(app);
+}
+
+class MyStatlessComponentRoot extends StatelessComponent {
+  RouteArguments args;
+  MyStatlessComponentRoot(this.args) {}
+  Widget build(BuildContext context) {
+    Widget content = new Material(child: new Text("ROOT", style: new TextStyle(fontSize: 100.0)));
+    GestureDetector gesture = new GestureDetector(child: content, onTap: () {
+      args.navigator.pushNamed("/image");
+    }, onLongPress: () {
+      args.navigator.pushNamed("/text");
+    });
+    return new Center(child: gesture);
+  }
+}
+
+class MyStatlessComponentImage extends StatelessComponent {
+  RouteArguments args;
+  MyStatlessComponentImage(this.args) {}
+  Widget build(BuildContext context) {
+    Widget content = new Material(child: new NetworkImage(src: 'icon.jpeg', width: 500.0, height: 500.0));
+    GestureDetector gesture = new GestureDetector(child: content, onTap: () {
+      args.navigator.pop();
+    });
+    return new Center(child: gesture);
+  }
+}
+
+class MyStatlessComponentText extends StatelessComponent {
+  RouteArguments args;
+  MyStatlessComponentText(this.args) {}
+  bool isPop = false;
+  Widget build(BuildContext context) {
+    Widget content = new Material(
+        child: new Text("Hello", style: new TextStyle(fontSize: 100.0)));
+    GestureDetector gesture = new GestureDetector(child: content, onTap: () {
+      args.navigator.pop();
+    });
+    return new Center(child: gesture);
+  }
+}
