e57f222019a1e51c1f2fae3632d4609361d027ac
kyorohiro <kyorohiro@gmail.com>
2015-10-25 22:42:39 +0900

    build android sample

---
+++ test_android/lib/main.dart
@@ -0,0 +1,21 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//library stocks;
+
+import 'dart:async';
+import 'dart:math' as math;
+import 'dart:ui' as ui;
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  Text t = new Text("Hello World");
+  Center c= new Center (child: t);
+  runApp(c);
+  //runApp(new StocksApp());
+}
