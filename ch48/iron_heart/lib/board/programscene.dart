9a3ca7583e7fedd566e022a3f295a19f2b749302
kyorohiro <kyorohiro@gmail.com>
2015-10-31 16:59:34 +0900

    ..

--- ch48/iron_heart/lib/board/programscene.dart
+++ ch48/iron_heart/lib/board/programscene.dart
@@ -17,14 +17,48 @@ class ProgramScree extends TinyDisplayObject {
       bgImg = i;
     });
     {
-      TinyButton button = new TinyButton("back_button", 200.0, 120.0, onPush);
-      button.mat = new Matrix4.translationValues(30.0, 480.0, 0.0);
-      button.bgcolorOn = new TinyColor.argb(0x22, 0xFF, 0x00, 0x00);
-      button.bgcolorOff =  new TinyColor.argb(0x00, 0x00, 0x00, 0xff);
-      button.bgcolorFocus = new TinyColor.argb(0x11, 0x00, 0xff, 0x00);
-      child.add(button);
+      child.add(newBackButton());
+      child.add(newSelectButton());
+      child.add(newYesButton());
+      child.add(newNoButton());
     }
     tipSelect = new TipSelect(this, selectTipX, selectTipY);
+  }
+
+  TinyDisplayObject newBackButton() {
+    TinyButton button = new TinyButton("back_button", 200.0, 120.0, onPush);
+    button.mat = new Matrix4.translationValues(30.0, 480.0, 0.0);
+    button.bgcolorOn = new TinyColor.argb(0x22, 0xFF, 0x00, 0x00);
+    button.bgcolorOff = new TinyColor.argb(0x00, 0x00, 0x00, 0xff);
+    button.bgcolorFocus = new TinyColor.argb(0x11, 0x00, 0xff, 0x00);
+    return button;
+  }
+
+  TinyDisplayObject newSelectButton() {
+    TinyButton button = new TinyButton("select_button", 80.0, 80.0, onPush);
+    button.mat = new Matrix4.translationValues(500.0, 500.0, 0.0);
+    button.bgcolorOn = new TinyColor.argb(0x22, 0xFF, 0x00, 0x00);
+    button.bgcolorOff = new TinyColor.argb(0xff, 0x55, 0x55, 0xff);
+    button.bgcolorFocus = new TinyColor.argb(0x11, 0x00, 0xff, 0x00);
+    return button;
+  }
+
+  TinyDisplayObject newYesButton() {
+    TinyButton button = new TinyButton("yes_button", 80.0, 80.0, onPush);
+    button.mat = new Matrix4.translationValues(600.0, 500.0, 0.0);
+    button.bgcolorOn = new TinyColor.argb(0x22, 0xFF, 0x00, 0x00);
+    button.bgcolorOff = new TinyColor.argb(0xff, 0x55, 0xff, 0xff);
+    button.bgcolorFocus = new TinyColor.argb(0x11, 0x00, 0xff, 0x00);
+    return button;
+  }
+
+  TinyDisplayObject newNoButton() {
+    TinyButton button = new TinyButton("no_button", 80.0, 80.0, onPush);
+    button.mat = new Matrix4.translationValues(700.0, 500.0, 0.0);
+    button.bgcolorOn = new TinyColor.argb(0x22, 0xFF, 0x00, 0x00);
+    button.bgcolorOff = new TinyColor.argb(0xff, 0xff, 0xff, 0x55);
+    button.bgcolorFocus = new TinyColor.argb(0x11, 0x00, 0xff, 0x00);
+    return button;
   }
 
   void onPaint(TinyStage stage, TinyCanvas canvas) {
@@ -34,33 +68,36 @@ class ProgramScree extends TinyDisplayObject {
 
   void drawBG(TinyStage stage, TinyCanvas canvas) {
     if (bgImg != null) {
-      TinyRect src = new TinyRect(0.0, 0.0, bgImg.w.toDouble(), bgImg.h.toDouble());
+      TinyRect src =
+          new TinyRect(0.0, 0.0, bgImg.w.toDouble(), bgImg.h.toDouble());
       TinyRect dst = new TinyRect(0.0, 0.0, 800.0, 600.0);
       TinyPaint p = new TinyPaint();
       canvas.drawImageRect(stage, bgImg, src, dst, p);
     }
   }
 
-  int selectTipX = 0;
-  int selectTipY = 0;
-  bool onTouch(TinyStage stage, int id, String type, double x, double y, double globalX, globalY) {
+  int selectTipX = 1;
+  int selectTipY = 1;
+  bool onTouch(TinyStage stage, int id, String type, double x, double y,
+      double globalX, globalY) {
     //print("###### x=${x}   y=${y}");
-    double x1 =x - 50.0;
-    double y1 =y - 5.0;
-    double xx = x1/(50+20);
-    double yy = y1/(50+20);
+    double x1 = x - 50.0;
+    double y1 = y - 5.0;
+    double xx = x1 / (50 + 20);
+    double yy = y1 / (50 + 20);
     //print("###### x:y=${xx}:${yy}   x=${x}   y=${y}");
-    selectTipX = xx.toInt();
-    selectTipY = yy.toInt();
-    if(!child.contains(tipSelect)) {
-      print(" #######-select-tip-##### ${selectTipX} ${selectTipY}");
-      if(0<selectTipX && selectTipX < game.environ.targetRed.program.w-1){
-        if(0<selectTipY && selectTipY < game.environ.targetRed.program.h-1){
-          child.add(tipSelect);
+
+    //
+    if (!child.contains(tipSelect)) {
+      int _tmpX = xx.toInt();
+      int _tmpY = yy.toInt();
+      if (0 < _tmpX && _tmpX < game.environ.targetRed.program.w - 1) {
+        if (0 < _tmpY && _tmpY < game.environ.targetRed.program.h - 1) {
+          selectTipX = xx.toInt();
+          selectTipY = yy.toInt();
         }
       }
     }
-    //
     return false;
   }
 
@@ -72,59 +109,75 @@ class ProgramScree extends TinyDisplayObject {
     }
   }
 
-
   void drawTip(TinyStage stage, TinyCanvas canvas, int x, int y) {
     TinyPaint p = new TinyPaint();
     p.strokeWidth = 2.5;
     p.style = TinyPaintStyle.stroke;
-    double xx = 50.0+x*(50+20);
-    double yy = 5.0+y*(50+20);
+    double xx = 50.0 + x * (50 + 20);
+    double yy = 5.0 + y * (50 + 20);
     double ww = 50.0;
     double hh = 50.0;
     TinyRect rect = new TinyRect(xx, yy, ww, hh);
     GameTip tip = game.environ.targetRed.program.getTip(x, y);
     p.color = new TinyColor(tip.id);
 
-    if(x == selectTipX && y == selectTipY) {
+    if (x == selectTipX && y == selectTipY) {
       p.style = TinyPaintStyle.fill;
     } else {
       p.style = TinyPaintStyle.stroke;
     }
     canvas.drawRect(stage, rect, p);
-    for(Next n in tip.dxys) {
+    for (Next n in tip.dxys) {
       double angle = 0.0;
-      if(n.dx==1 &&n.dy==0) {angle =   0.0;}
-      if(n.dx==1 &&n.dy==1) {angle =  45.0;}
-      if(n.dx==0 &&n.dy==1) {angle =  90.0;}
-      if(n.dx==-1 &&n.dy==1){angle = 135.0;}
-      if(n.dx==-1&&n.dy==0) {angle =  180.0;}
-      if(n.dx==-1&&n.dy==-1) {angle =  215.0;}
-      if(n.dx==0&&n.dy==-1) {angle =  260.0;}
-      if(n.dx==1&&n.dy==-1) {angle =  315.0;}
-      drawArrow(stage, canvas, x, y, math.PI*2.0*((angle-90.0)/360.0));
+      if (n.dx == 1 && n.dy == 0) {
+        angle = 0.0;
+      }
+      if (n.dx == 1 && n.dy == 1) {
+        angle = 45.0;
+      }
+      if (n.dx == 0 && n.dy == 1) {
+        angle = 90.0;
+      }
+      if (n.dx == -1 && n.dy == 1) {
+        angle = 135.0;
+      }
+      if (n.dx == -1 && n.dy == 0) {
+        angle = 180.0;
+      }
+      if (n.dx == -1 && n.dy == -1) {
+        angle = 215.0;
+      }
+      if (n.dx == 0 && n.dy == -1) {
+        angle = 260.0;
+      }
+      if (n.dx == 1 && n.dy == -1) {
+        angle = 315.0;
+      }
+      drawArrow(stage, canvas, x, y, math.PI * 2.0 * ((angle - 90.0) / 360.0));
     }
   }
 
-  void drawArrow(TinyStage stage, TinyCanvas canvas, int x, int y, double angle) {
+  void drawArrow(
+      TinyStage stage, TinyCanvas canvas, int x, int y, double angle) {
     TinyPaint p = new TinyPaint();
     p.strokeWidth = 2.5;
     p.style = TinyPaintStyle.stroke;
     p.color = new TinyColor(game.environ.targetRed.program.getTip(x, y).id);
 
-    double xx = 50.0+x*(50+20);
-    double yy = 5.0+y*(50+20);
+    double xx = 50.0 + x * (50 + 20);
+    double yy = 5.0 + y * (50 + 20);
     double ww = 50.0;
     double hh = 50.0;
 
     //
     // allow
     TinyPoint p1 = new TinyPoint(-10.0, 0.0);
-    TinyPoint p2 = new TinyPoint(-10.0, hh/2+(20.0*5/6.0));
-    TinyPoint p3 = new TinyPoint(-10.0-ww*1/5, hh/2+(20.0*2/3.0));
-    TinyPoint p4 = new TinyPoint(-10.0+ww*1/5, hh/2+(20.0*2/3.0));
+    TinyPoint p2 = new TinyPoint(-10.0, hh / 2 + (20.0 * 5 / 6.0));
+    TinyPoint p3 = new TinyPoint(-10.0 - ww * 1 / 5, hh / 2 + (20.0 * 2 / 3.0));
+    TinyPoint p4 = new TinyPoint(-10.0 + ww * 1 / 5, hh / 2 + (20.0 * 2 / 3.0));
     Matrix4 mat = new Matrix4.identity();
 
-    mat.translate(xx+ww/2,yy+hh/2,0.0);
+    mat.translate(xx + ww / 2, yy + hh / 2, 0.0);
     mat.rotateZ(angle);
     canvas.pushMulMatrix(mat);
     canvas.drawLine(stage, p1, p2, p);
@@ -134,7 +187,21 @@ class ProgramScree extends TinyDisplayObject {
   }
 
   void onPush(String id) {
-    game.stage.root.clearChild();
-    game.stage.root.addChild(game.playScene);
+    print("id == ${id}");
+    switch (id) {
+      case "select_button":
+        if (!child.contains(tipSelect)) {
+          child.add(tipSelect);
+        }
+        break;
+      case "back_button":
+        game.stage.root.clearChild();
+        game.stage.root.addChild(game.playScene);
+        break;
+      case "yes_button":
+      break;
+      case "no_button":
+      break;
+    }
   }
 }
