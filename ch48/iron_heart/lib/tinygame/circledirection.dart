fb4d0ec8101afc5e74b563065b3fc8b82682c76e
kyorohiro <kyorohiro@gmail.com>
2015-11-01 20:17:08 +0900

    ..

--- ch48/iron_heart/lib/tinygame/circledirection.dart
+++ ch48/iron_heart/lib/tinygame/circledirection.dart
@@ -11,45 +11,88 @@ class TinyCircleDirection extends TinyDisplayObject {
 
   bool onTouch(TinyStage stage, int id, String type, double x, double y,
       double globalX, globalY) {
+    onTouchCircle(stage, id, type, x, y, globalX, globalY);
+    onTouchRange(stage, id, type, x, y, globalX, globalY);
+  }
+
+  bool onTouchCircle(TinyStage stage, int id, String type, double x, double y,
+      double globalX, globalY) {
     double cx = viewWidth / 2.0;
     double cy = viewWidth / 2.0;
     if (math.sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy)) <
         viewWidth / 2.0) {
-      angle = math.atan2(y - cy, x - cx) + math.PI / 2;
+        angle = math.atan2(y - cy, x - cx) + math.PI / 2;
     }
   }
-
+  bool onTouchRange(TinyStage stage, int id, String type, double x, double y,
+      double globalX, globalY) {
+    double cx = viewWidth+ rangeWidth/2;
+    double cy = 0.0;
+    if(viewWidth<x && x<(viewWidth+rangeWidth)) {
+      if(0<y && y<viewWidth) {
+         range = (y/viewWidth)*math.PI;
+      }
+    }
+  }
   void onPaint(TinyStage stage, TinyCanvas canvas) {
     paintCircle(stage, canvas);
+    paintRange(stage, canvas);
+  }
+
+  paintRange(TinyStage stage, TinyCanvas canvas) {
+    TinyPaint p = new TinyPaint();
+    p.style = TinyPaintStyle.stroke;
+    p.strokeWidth = rangeWidth/3;
+    p.color = fgColor;
+    double cx = viewWidth+ rangeWidth/2;
+    double cy = 0.0;
+    canvas.drawLine(
+        stage,
+        new TinyPoint(cx, cy),
+        new TinyPoint(cx, cy+viewWidth),
+        p);
+    canvas.drawLine(
+            stage,
+            new TinyPoint(cx, cy+viewWidth*(range/math.PI)-10.0),
+            new TinyPoint(cx, cy+viewWidth*(range/math.PI)+10.0),
+            p);
   }
 
   paintCircle(TinyStage stage, TinyCanvas canvas) {
     TinyPaint p = new TinyPaint();
     p.style = TinyPaintStyle.stroke;
-    p.strokeWidth = 5.0;
+
     TinyRect rect = new TinyRect(0.0, 0.0, viewWidth, viewWidth);
     canvas.drawOval(stage, rect, p);
 
     p.color = fgColor;
     double cx = viewWidth / 2.0;
     double cy = viewWidth / 2.0;
+
+    p.strokeWidth = 15.0;
     canvas.drawLine(
         stage,
         new TinyPoint(cx, cy),
-        new TinyPoint(cx + cx * math.cos(angle - math.PI / 2),
-            cy + cy * math.sin(angle - math.PI / 2)),
+        new TinyPoint(
+          cx + cx * math.cos(angle - math.PI / 2),
+          cy + cy * math.sin(angle - math.PI / 2)),
         p);
+
+    p.strokeWidth = 2.5;
     canvas.drawLine(
         stage,
         new TinyPoint(cx, cy),
-        new TinyPoint(cx + cx * math.cos(angle -range- math.PI / 2),
-            cy + cy * math.sin(angle -range- math.PI / 2)),
+        new TinyPoint(
+          cx + cx * math.cos(angle -range- math.PI / 2),
+          cy + cy * math.sin(angle -range- math.PI / 2)),
         p);
+    p.strokeWidth = 2.5;
     canvas.drawLine(
         stage,
         new TinyPoint(cx, cy),
-        new TinyPoint(cx + cx * math.cos(angle +range- math.PI / 2),
-            cy + cy * math.sin(angle +range- math.PI / 2)),
+        new TinyPoint(
+          cx + cx * math.cos(angle +range- math.PI / 2),
+          cy + cy * math.sin(angle +range- math.PI / 2)),
         p);
   }
 /*
