b6b9d592d22546f417f7ea194fc8582920d540e3
kyorohiro <kyorohiro@gmail.com>
2015-11-02 19:01:48 +0900

    ..

--- ch48/iron_heart/lib/tinyphysics2d/world.dart
+++ ch48/iron_heart/lib/tinyphysics2d/world.dart
@@ -18,6 +18,28 @@ class World {
     }
   }
 
+  List<Primitive> searchPrimitive(Primitive base, double direction, double range, double startDist, double endDist) {
+    double s2 = normalizeAngle(direction);
+    double starting = s2 - range;
+    double ending = s2 + range;
+
+    List<Primitive> ret = [];
+    for (Primitive t in primitives) {
+      double d = World.distance(base, t);
+      double a = World.angleFromP2(t, base);
+      if(!(startDist<=d && d<=endDist)) {
+        continue;
+      }
+      double ss = a - starting;
+      double ee = a - ending;
+      if (ss >= 0 && ee <= 0) {
+        ret.add(t);
+      } else {
+      }
+    }
+    return ret;
+  }
+
   static double distance(Primitive p1, Primitive p2) {
     double x = p1.xy.x - p2.xy.x;
     double y = p1.xy.y - p2.xy.y;
@@ -29,4 +51,15 @@ class World {
     double y = p1.xy.y - p2.xy.y;
     return math.atan2(y, x);
   }
+
+  static double normalizeAngle(double a) {
+    a += math.PI * 2 * 4;
+    a = a % (2 * math.PI);
+    if (a < math.PI) {
+      return a;
+    } else {
+      return -math.PI + (a - math.PI);
+    }
+  }
+
 }
