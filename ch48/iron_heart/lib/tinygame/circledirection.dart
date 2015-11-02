2637e10e4f2dc94cc27e9eef33d99a27c9aa8aa4
kyorohiro <kyorohiro@gmail.com>
2015-11-02 16:02:34 +0900

    ..

--- ch48/iron_heart/lib/tinygame/circledirection.dart
+++ ch48/iron_heart/lib/tinygame/circledirection.dart
@@ -26,35 +26,39 @@ class TinyCircleDirection extends TinyDisplayObject {
     double cy = circleSize / 2.0;
     if (math.sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy)) <
         circleSize / 2.0) {
-        angle = math.atan2(y - cy, x - cx) + math.PI / 2;
-        return true;
+      angle = math.atan2(y - cy, x - cx) + math.PI / 2;
+      return true;
     }
     return false;
   }
+
   bool onTouchRange(TinyStage stage, int id, String type, double x, double y,
       double globalX, globalY) {
-    double cx = circleSize+ rangeWidth/2;
+    double cx = circleSize + rangeWidth / 2;
     double cy = 0.0;
-    if(circleSize<x && x<(circleSize+rangeWidth)) {
-      if(0<y && y<circleSize) {
-         range = (y/circleSize)*math.PI;
-         return true;
+    if (circleSize < x && x < (circleSize + rangeWidth)) {
+      if (0 < y && y < circleSize) {
+        range = (y / circleSize) * math.PI;
+        return true;
       }
     }
     return false;
   }
+
   bool onTouchDistance(TinyStage stage, int id, String type, double x, double y,
       double globalX, globalY) {
-    double cx = circleSize+ rangeWidth+rangeWidth/2;
+    double cx = circleSize + rangeWidth + rangeWidth / 2;
     double cy = 0.0;
-    if(circleSize+rangeWidth<x && x<(circleSize+rangeWidth+rangeWidth)) {
-      if(0<y && y<circleSize) {
-         distance = (y/circleSize);
-         return true;
+    if (circleSize + rangeWidth < x &&
+        x < (circleSize + rangeWidth + rangeWidth)) {
+      if (0 < y && y < circleSize) {
+        distance = (y / circleSize);
+        return true;
       }
     }
     return false;
   }
+
   void onPaint(TinyStage stage, TinyCanvas canvas) {
     paintCircle(stage, canvas);
     paintRange(stage, canvas);
@@ -64,39 +68,35 @@ class TinyCircleDirection extends TinyDisplayObject {
   paintRange(TinyStage stage, TinyCanvas canvas) {
     TinyPaint p = new TinyPaint();
     p.style = TinyPaintStyle.stroke;
-    p.strokeWidth = rangeWidth/3;
+    p.strokeWidth = rangeWidth / 3;
     p.color = fgColor;
-    double cx = circleSize+ rangeWidth/2;
+    double cx = circleSize + rangeWidth / 2;
     double cy = 0.0;
     canvas.drawLine(
-        stage,
-        new TinyPoint(cx, cy),
-        new TinyPoint(cx, cy+circleSize),
-        p);
+        stage, new TinyPoint(cx, cy), new TinyPoint(cx, cy + circleSize), p);
     canvas.drawLine(
-            stage,
-            new TinyPoint(cx, cy+circleSize*(range/math.PI)-10.0),
-            new TinyPoint(cx, cy+circleSize*(range/math.PI)+10.0),
-            p);
+        stage,
+        new TinyPoint(cx, cy + circleSize * (range / math.PI) - 10.0),
+        new TinyPoint(cx, cy + circleSize * (range / math.PI) + 10.0),
+        p);
   }
+
   paintDistance(TinyStage stage, TinyCanvas canvas) {
     TinyPaint p = new TinyPaint();
     p.style = TinyPaintStyle.stroke;
-    p.strokeWidth = rangeWidth/3;
+    p.strokeWidth = rangeWidth / 3;
     p.color = fgColor;
-    double cx = circleSize+rangeWidth+ rangeWidth/2;
+    double cx = circleSize + rangeWidth + rangeWidth / 2;
     double cy = 0.0;
     canvas.drawLine(
-        stage,
-        new TinyPoint(cx, cy),
-        new TinyPoint(cx, cy+circleSize),
-        p);
+        stage, new TinyPoint(cx, cy), new TinyPoint(cx, cy + circleSize), p);
     canvas.drawLine(
-            stage,
-            new TinyPoint(cx, cy+circleSize*(distance)-10.0),
-            new TinyPoint(cx, cy+circleSize*(distance)+10.0),
-            p);
+        stage,
+        new TinyPoint(cx, cy + circleSize * (distance) - 10.0),
+        new TinyPoint(cx, cy + circleSize * (distance) + 10.0),
+        p);
   }
+
   paintCircle(TinyStage stage, TinyCanvas canvas) {
     TinyPaint p = new TinyPaint();
     p.style = TinyPaintStyle.stroke;
@@ -112,26 +112,39 @@ class TinyCircleDirection extends TinyDisplayObject {
     canvas.drawLine(
         stage,
         new TinyPoint(cx, cy),
-        new TinyPoint(
-          cx + distance*cx * math.cos(angle - math.PI / 2),
-          cy + distance*cy * math.sin(angle - math.PI / 2)),
+        new TinyPoint(cx + distance * cx * math.cos(angle - math.PI / 2),
+            cy + distance * cy * math.sin(angle - math.PI / 2)),
         p);
 
+    double px = cx;
+    double py = cy;
+    for(int i=0;i<20;i++) {
+      p.strokeWidth = 2.5;
+      double qx = cx + distance * cx * math.cos(angle - range +range*2*(i/19.0)- math.PI / 2);
+      double qy = cy + distance * cy * math.sin(angle - range +range*2*(i/19.0)- math.PI / 2);
+      canvas.drawLine(
+          stage,
+          new TinyPoint(px, py),
+          new TinyPoint(qx, qy),
+          p);
+      px = qx;
+      py = qy;
+    }
     p.strokeWidth = 2.5;
     canvas.drawLine(
         stage,
         new TinyPoint(cx, cy),
         new TinyPoint(
-          cx + distance * cx * math.cos(angle -range- math.PI / 2),
-          cy + distance * cy * math.sin(angle -range- math.PI / 2)),
+            cx + distance * cx * math.cos(angle - range - math.PI / 2),
+            cy + distance * cy * math.sin(angle - range - math.PI / 2)),
         p);
     p.strokeWidth = 2.5;
     canvas.drawLine(
         stage,
         new TinyPoint(cx, cy),
         new TinyPoint(
-          cx + distance * cx * math.cos(angle +range- math.PI / 2),
-          cy + distance * cy * math.sin(angle +range- math.PI / 2)),
+            cx + distance * cx * math.cos(angle + range - math.PI / 2),
+            cy + distance * cy * math.sin(angle + range - math.PI / 2)),
         p);
   }
 }
