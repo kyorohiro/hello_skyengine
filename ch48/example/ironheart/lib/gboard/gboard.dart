1e44f10fdb0da45dbc90ee325c2aa33cfc0d1cf4
kyorohiro <kyorohiro@gmail.com>
2015-11-05 22:25:49 +0900

    ..

--- ch48/example/ironheart/lib/gboard/gboard.dart
+++ ch48/example/ironheart/lib/gboard/gboard.dart
@@ -1,9 +1,11 @@
 library gboard;
-import 'package:flutter/widgets.dart';
-import 'package:flutter/rendering.dart';
+//import 'package:flutter/widgets.dart';
+//import 'package:flutter/rendering.dart';
 import 'package:ch48/tinygame.dart';
 import 'dart:math' as math;
 import '../glogic/glogic.dart';
+import 'package:vector_math/vector_math_64.dart';
+
 part 'programscene.dart';
 part 'rangeselect.dart';
 part 'setting.dart';
---
+++ ch48/example/ironheart/web/main.dart
@@ -0,0 +1,15 @@
+import 'dart:html';
+import 'dart:web_gl';
+import 'dart:math' as  math;
+import 'package:ch48/tinygame.dart';
+import 'package:ch48/tinygame_webgl.dart';
+import 'package:vector_math/vector_math_64.dart';
+
+void main() {
+  print("--------1-dart hello ( 1 )");
+  TinyGameBuilderForWebgl builder = new TinyGameBuilderForWebgl();
+  // TinyGameBuilderForWebgl gl = new TinyGameBuilderForWebgl();
+  // TinyStage stage = gl.createStage(new TinyGameRoot(400.0, 400.0));
+  // stage.start();
+  print("--------1-dart hello ( 2 ) ");
+}
