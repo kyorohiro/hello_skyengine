1ec7c8a93d8f38644d9b7e9c2c2147287495196b
kyorohiro <kyorohiro@gmail.com>
2015-11-05 22:55:39 +0900

    ..

--- ch48/example/ironheart/lib/glogic/glogic.dart
+++ ch48/example/ironheart/lib/glogic/glogic.dart
@@ -4,10 +4,9 @@ import 'dart:math' as math;
 import 'dart:async';
 import 'package:ch48/tinyphysics2d.dart';
 import 'package:ch48/tinygame.dart';
-import 'package:ch48/tinygame_flutter.dart';
+//import 'package:ch48/tinygame_flutter.dart';
 import '../gplay/gplay.dart';
 import '../gboard/gboard.dart';
-import '../main.dart';
 import '../gopen/startscene.dart';
 
 part 'env.dart';
--- ch48/example/ironheart/lib/gplay/gplay.dart
+++ ch48/example/ironheart/lib/gplay/gplay.dart
@@ -1,8 +1,7 @@
 library gplay;
-import 'package:flutter/widgets.dart';
-import 'package:flutter/rendering.dart';
 import 'package:ch48/tinygame.dart';
 import '../glogic/glogic.dart';
+import 'package:vector_math/vector_math_64.dart';
 part 'playbullet.dart';
 part 'playchara.dart';
 part 'playscene.dart';
