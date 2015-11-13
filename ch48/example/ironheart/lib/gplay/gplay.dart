5103c2e1abe9746d2793c447a526f79df7f68c70
kyorohiro@unemployed <kyorohiro@gmail.com>
2015-11-13 22:00:23 +0900

    Delete ch48/example/ironheart/lib/gplay/gplay.dart

--- ch48/example/ironheart/lib/gplay/gplay.dart
+++
@@ -1,7 +0,0 @@
-library gplay;
-import 'package:ch48/tinygame.dart';
-import '../glogic/glogic.dart';
-import 'package:vector_math/vector_math_64.dart';
-part 'playbullet.dart';
-part 'playchara.dart';
-part 'playscene.dart';
