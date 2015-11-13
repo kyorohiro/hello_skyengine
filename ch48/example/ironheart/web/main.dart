9271676d440065a2db5bd01b86e38b530259766e
kyorohiro@unemployed <kyorohiro@gmail.com>
2015-11-13 21:57:20 +0900

    Delete ch48/example/ironheart/web/main.dart

--- ch48/example/ironheart/web/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ch48/tinygame_webgl.dart';
-import 'package:ironheart/glogic/glogic.dart';
-void main() {
-  print("--------1-dart hello ( 1 )");
-  Game game = new Game(new TinyGameBuilderForWebgl());
-  game.stage.start();
-  game.stage.root.child.add(game.startScene);
-  print("--------1-dart hello ( 2 ) ");
-}
