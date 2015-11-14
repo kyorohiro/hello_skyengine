8599d54bf28f83272792eed1f57046c1d71fb3f2
kyorohiro <kyorohiro@gmail.com>
2015-11-14 19:09:32 +0900

    ..

---
+++ widget_rawimage/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+//import 'dart:ui' as sky;
+import 'dart:async';
+import 'dart:typed_data';
+import 'package:mojo/core.dart' as core;
+
+main() async {
+  final double width = 300.0;
+  final double height = 300.0;
+  final ColorFilter colorFilter =
+   new ColorFilter.mode(new Color.fromARGB(0xaa, 0xff, 0xaa, 0xaa), TransferMode.color);
+  //final ImageFit fit = ImageFit.fill;
+  //final ImageRepeat repeat = ImageRepeat.repeat;
+  //final Rect centerSlice = new Rect.fromLTWH(0.0, 0.0, 20.0, 20.0);
+  ByteData data = await ResourceLoader.load("assets/icon.jpeg");
+
+  RawImage i = new RawImage(
+    bytes:data.buffer.asUint8List(),
+    width:width,height:height,
+    colorFilter:colorFilter);
+  runApp(new Center(child:i));
+}
+
+
+class ResourceLoader {
+  static AssetBundle getAssetBundle() {
+    if (rootBundle != null) {
+      return rootBundle;
+    } else {
+      return new NetworkAssetBundle(new Uri.directory(Uri.base.origin));
+    }
+  }
+
+  static Future<ByteData> load(String url) async {
+    AssetBundle bundle = getAssetBundle();
+    core.MojoDataPipeConsumer c = await bundle.load(url);
+    return await core.DataPipeDrainer.drainHandle(c);
+  }
+}
