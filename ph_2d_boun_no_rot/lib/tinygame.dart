57912622d4f52a5d3f945ed6b3ecc103728af0f9
kyorohiro <kyorohiro@gmail.com>
2015-12-14 01:08:56 +0900

    checkgit add -A

--- ph_2d_boun_no_rot/lib/tinygame.dart
+++ ph_2d_boun_no_rot/lib/tinygame.dart
@@ -3,6 +3,7 @@ library tinygame;
 import 'package:flutter/widgets.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/animation.dart';
+import 'package:flutter/scheduler.dart';
 import 'dart:math' as math;
 import 'dart:ui' as sky;
 import 'dart:async';
