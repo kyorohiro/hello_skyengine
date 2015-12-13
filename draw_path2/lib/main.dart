a7180c460ca13533c8b223d108afe46290516a97
kyorohiro <kyorohiro@gmail.com>
2015-12-13 22:12:33 +0900

    checkgit add -A

--- draw_path2/lib/main.dart
+++ draw_path2/lib/main.dart
@@ -16,10 +16,21 @@ class DrawPathWidget extends OneChildRenderObjectWidget {
 class DrawPathObject extends RenderBox {
   void paint(PaintingContext context, Offset offset) {
     context.canvas.scale(2.5, 2.5);
+    //context.canvas.save();
+    context.canvas.rotate(3.14/8);
+    //context.canvas.clipRect(new Rect.fromLTWH(0.0, 0.0, 100.0, 100.0));
+
+    clipWithStroke(context, offset, 2.0);
+    //Rect r = new Rect.fromLTWH(0.0, 0.0, 500.0, 500.0);
+    //lipWithStroke(context, offset, 1.0);
     context.canvas.translate(50.0, 50.0);
     paintWithStroke(context, offset);
+    //context.canvas.restore();
+    //context.canvas.clipRect(r);
+
     context.canvas.translate(50.0, 0.0);
     //paintWithLinearGradient(context, offset);
+
     paintWithFill(context, offset);
   }
   void paintWithFill(PaintingContext context, Offset offset) {
@@ -58,6 +69,24 @@ class DrawPathObject extends RenderBox {
     path.close();
     p.color = new Color.fromARGB(0xaa, 0xaa, 0xff, 0xff);
     context.canvas.drawPath(path, p);
+  }
+
+  void clipWithStroke(PaintingContext context, Offset offset, double v) {
+    Paint p = new Paint();
+    p.strokeWidth = 2.0;
+    p.style = sky.PaintingStyle.stroke;
+    p.filterQuality = sky.FilterQuality.high;
+    //    p.colorFilter =new ColorFilter.mode(color, transferMode)
+    p.strokeCap = sky.StrokeCap.butt;
+    Path path = new Path();
+    path.shift(new Offset(50.0, 50.0));
+    path.moveTo(0.0, 0.0);
+    path.lineTo(0.0, 100.0*v);
+    path.lineTo(100.0*v, 100.0*v);
+    path.lineTo(100.0*v, 0.0);
+    path.close();
+    p.color = new Color.fromARGB(0xaa, 0xaa, 0xff, 0xff);
+    context.canvas.clipPath(path);
   }
 
 }
